2bf6e0de1a76e7dff952b3e2149e2e5ac52914cb
cosmiminaut <38096276+cosmiminaut@users.noreply.github.com>
2019-09-18 01:37:15 +0500

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.route('/test')
 app.get('/article', function (req, res) {
     res.sendFile(__dirname + '/public/article.html');
 });
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');    
 });
